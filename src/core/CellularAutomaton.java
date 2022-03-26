4e296a8c6782de6f43e20e1f69144f5b665ec24d
Joaquín García del Río <jogarcia@itba.edu.ar>
2022-03-26 15:54:06 -0300

    funcion gameOver() y buscado de vecinos pero incompleta.

--- src/core/CellularAutomaton.java
+++ src/core/CellularAutomaton.java
@@ -5,42 +5,43 @@ import java.util.List;
 
 public class CellularAutomaton {
     private int totalCell;
-    private Space space;
+    private int size = 100;
+    private Space space1,space2;
 
     public CellularAutomaton(String staticFile, String dynamicFile){
         //TODO:hacer
         //lee del static el size y si el 3D o no
-        //space = new Space(size, true);
+        space1 = new Space(size, true);
+        space2 = new Space(size, true);
 
         //TODO:hacer
-        //a mediada que lee el dynamic llena el spacio
-        space.add(10, 1, 1);
+        //a mediada que lee el dynamic llena el spacio1
+        space1.add(10, 1, 1);
     }
 
     public void solve(Rules rules){
         int t=0;
         while (check()){
             List<Cell> liveCell = new ArrayList<>(); //donde se van guardando las celdas vivas
-            while (space.hasNext()){
-                Cell currentCell = space.next();
-                int numLiveNeighCells = space.getNumLiveNeighborsCell(currentCell.getPnt());
+            while (space1.hasNext()){
+                Cell currentCell = space1.next();
+                int numLiveNeighCells = space1.getNumLiveNeighborsCell(currentCell.getPnt().getX(),currentCell.getPnt().getY(),currentCell.getPnt().getZ());
                 if(rules.apply(currentCell, numLiveNeighCells)){
                    liveCell.add(currentCell);
                 }
             }
-            space.update(liveCell);
+            space1.update(liveCell);
             writeOutput(liveCell, t);
             t++;
         }
     }
 
     private boolean check(){
-        //TODO:hacer
         //chequea si se puede seguir con el algoritmo
         //-si no hay celdas vivas terminas
         //si hace varios ciclos no hay celldas nuevas termina
         //si una celda borde vive se termina
-        return true;
+        return space1.gameOver();
     }
 
 
--- src/core/Space.java
+++ src/core/Space.java
@@ -1,13 +1,16 @@
 package core;
 
+import javafx.geometry.Point3D;
+
 import java.util.Iterator;
 import java.util.List;
 
 public class Space implements Iterator<Cell>{
-
+    private static final int  centerX = 50, centerY = 50, centerZ = 50;
     private final Node[][][] space;
     private final int size;
     private final boolean treeD; //3D
+    private final Point3D center;
 
     public Space(int size, boolean treeD ) {
         this.treeD = treeD;
@@ -17,6 +20,7 @@ public class Space implements Iterator<Cell>{
         }else{
             space = new Node[size][size][0];
         }
+        center = new Point3D(centerX, centerY ,centerZ);
         //TODO: creamos todo los nodos y los dejamos muertos
     }
 
@@ -31,15 +35,81 @@ public class Space implements Iterator<Cell>{
     }
 
 
-    public int getNumLiveNeighborsCell(Point pnt){
+    public int getNumLiveNeighborsCell(int x,int y,  int z){
+        int toRet = 0;
+
+
+
+        if(x>0){ //creo que faltan
+            toRet += space[x-1][y][z].isAlive() ? 1:0;           // x-1 y z
+            if(y>0){
+                toRet += space[x-1][y-1][z].isAlive() ? 1:0;     // x-1 y-1 z
+                if(z >0){
+                    toRet += space[x-1][y-1][z-1].isAlive() ? 1:0;  // x-1 y-1 z-1
+                }
+                if(z < size-1 && treeD){
+                    toRet += space[x-1][y-1][z+1].isAlive() ? 1:0;   // x-1 y-1 z+1
+                }
+            }
+            if(y < size-1){
+                toRet += space[x-1][y+1][z].isAlive() ? 1:0;    // x-1 y+1 z
+                if(z >0){
+                    toRet += space[x-1][y+1][z-1].isAlive() ? 1:0;  // x-1 y+1 z-1
+                }
+                if(z < size-1 && treeD){
+                    toRet += space[x-1][y+1][z+1].isAlive() ? 1:0;   // x-1 y+1 z+1
+                }
+            }
+
+
+            if(z > 0){
+                toRet += space[x-1][y][z-1].isAlive() ? 1:0;    // x-1 y z-1
+            }
+            if(z < size-1 && treeD){
+                toRet += space[x-1][y][z+1].isAlive() ? 1:0;   // x-1 y z+1
+            }
+        }
+
+        if(y>0){
+            toRet += space[x][y-1][z].isAlive() ? 1:0;       // x y-1 z
+            if(x < size-1){
+                toRet += space[x+1][y-1][z].isAlive() ? 1:0;    // x+1 y-1 z
+                if(z > 0){
+                    toRet += space[x+1][y-1][z-1].isAlive() ? 1:0;    // x+1 y-1 z-1
+                }
+                if(z < size-1 && treeD){
+                    toRet += space[x+1][y-1][z+1].isAlive() ? 1:0;   // x+1 y-1 z+1
+                }
+            }
+            if(z > 0){
+                toRet += space[x][y-1][z-1].isAlive() ? 1:0;    // x y-1 z-1
+            }
+            if(z < size-1 && treeD){
+                toRet += space[x][y-1][z+1].isAlive() ? 1:0;   // x y-1 z+1
+            }
+        }
+
+        if(z>0){
+            toRet += space[x][y][z-1].isAlive() ? 1:0; // x y z-1
+            if(y < size-1){
+                toRet += space[x][y+1][z-1].isAlive() ? 1:0; // x y+1 z-1
+            }
+            if(x<size-1){
+                toRet += space[x+1][y][z-1].isAlive() ? 1:0; // x+1 y z-1
+                if(y < size-1){
+                    toRet += space[x+1][y+1][z-1].isAlive() ? 1:0; //x+1 y+1 z-1
+                }
+            }
+
+        }
         //TODO:hacer
         return 0;
     }
 
-    private int getDistOrigin(int x, int y, int z){
+    private double getDistOrigin(int x, int y, int z){
         //TODO:hacer
         //dada una posicion devulve la posicion al origen
-        return 0;
+        return center.distance( x, y, z);
     }
 
 
@@ -53,6 +123,39 @@ public class Space implements Iterator<Cell>{
     public Cell next() {
         //TODO:hacer
         return null;
+    }
+
+
+    public boolean gameOver(){
+        boolean allAreDead = true,
+                spaceDidntChange = false; //lo pongo en false pero tiene que ser true. habría que comparar tn con tn+1 y si difieren entonces apagar el flag
+        if(treeD) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    for (int k = 0; k < size; k++) {
+                        if (space[i][j][k].isAlive()) {
+                            allAreDead = false;
+                            if ((k == 0 || j == 0 || i == 0 || k == size - 1 || j == size - 1 || i == size - 1)) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (space[i][j][0].isAlive()) {
+                        allAreDead = false;
+                        if ((j == 0 || i == 0 || j == size - 1 || i == size - 1)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return allAreDead || spaceDidntChange;
     }
 
 
